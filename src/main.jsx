058c53815e9bf2499f532dfca8fc08958189abc2
nayeong lee <lny021102@gmail.com>
2024-10-11 00:13:01 +0900

    [ADD] 모바일, 태블릿 접속 차단

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// 모바일 및 태블릿 장치 감지
+const isMobileOrTablet = /Mobi|Android|iPhone|iPad|iPod|Tablet/i.test(navigator.userAgent);
+
+if (isMobileOrTablet) {
+  // 모바일 또는 태블릿일 경우 접근 차단
+  document.body.innerHTML = '<h1>모바일 및 태블릿 장치로는 이 페이지에 접속할 수 없습니다.</h1>';
+} else {
+  // PC일 경우에만 React 앱을 렌더링
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
